682a4267ebdc58e6abbc675a6738a2e75fe10eae
roytravel <roytravel97@gmail.com>
2021-11-23 10:32:18 +0900

    Add tar compression function

---
+++ run_compress.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+import os
+import tarfile
+
+def get_file(directory) -> list:
+    fullpaths = []
+    for (root, dir, files) in os.walk(directory):
+        root = root + '/'
+        for _file in files:
+            if _file.endswith('.ann') and _file[0] is not ".":
+                fullpath = root + _file
+                fullpaths.append(fullpath)
+    return fullpaths
+            
+
+def make_tar(filepaths) -> None:
+    with tarfile.open("Article.tar.gz" ,mode="w:gz") as tar:
+        for _file in filepaths:
+            tar.add(_file)
+
+
+def run() -> None:
+    filepaths = get_file('cti-articles/')
+    make_tar(filepaths)
+
+
+if __name__ == '__main__':
+    run()
